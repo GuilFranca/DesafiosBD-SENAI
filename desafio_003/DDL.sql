6f48b12e0b9468b4ecd834af00dd792ed995b933
GuilFranca <guilfranca27@gmail.com>
2025-04-02 10:38:15 -0300

    att desafio 3

---
+++ desafio_003/DDL.sql
@@ -0,0 +1,22 @@
+CREATE DATABASE IF NOT EXISTS dbpesquisa;
+USE dbpesquisa;
+
+CREATE TABLE IF NOT EXISTS ruidos (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nivel INT NOT NULL,
+    horario TIME NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS cruzamentos (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    endereco VARCHAR(255) NOT NULL,
+    nome VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS niveis_medios (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    ruido_id INT NOT NULL,
+    cruzamento_id INT NOT NULL,
+    CONSTRAINT fk_ruido_id FOREIGN KEY (ruido_id) REFERENCES ruidos(id) ON DELETE CASCADE,
+    CONSTRAINT fk_cruzamento_id FOREIGN KEY (cruzamento_id) REFERENCES cruzamentos(id) ON DELETE CASCADE
+);
---
+++ desafio_003/DQL.sql
@@ -0,0 +1,5 @@
+SELECT AVG(nivel) AS media_nivel FROM ruidos;
+SELECT COUNT(nivel) AS total_registros FROM ruidos;
+SELECT SUM(nivel) AS soma_nivel FROM ruidos;
+SELECT MIN(nivel) AS menor_nivel FROM ruidos;
+SELECT MAX(nivel) AS maior_nivel FROM ruidos;
